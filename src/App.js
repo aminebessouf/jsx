f182e7bb3214a0d79c8d50359442d71963705c73
aminebessouf <amine.bessouf@gmail.com>
2022-04-28 16:37:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import './Style.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 class="title ">amine</h1>
+        <img src="C:\Users\amine\react-jsx\public\images.jpg" alt='2' />
+        <img src='../public/images.jpg' alt='2'/>
+      </div>
+        <video src="myVideo.mp4" type="video/mp4"  width="320" height="240" controls />
+    </div>
+  );
+}
+
+export default App;
